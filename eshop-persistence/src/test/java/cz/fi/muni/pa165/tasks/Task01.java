50e2d5664132dbe7922831caa6e5648b090c21c2
Jirka Oliva <o.krysa@seznam.cz>
2018-10-22 19:45:54 +0200

    Update

--- eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task01.java
+++ eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task01.java
@@ -12,9 +12,11 @@ import org.testng.annotations.Test;
 import cz.fi.muni.pa165.PersistenceSampleApplicationContext;
 import cz.fi.muni.pa165.entity.Category;
 
+import static org.testng.Assert.assertEquals;
 
 
-public class Task01  {
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class Task01 extends AbstractTestNGSpringContextTests{
 
 	
 	@PersistenceUnit
@@ -30,6 +32,15 @@ public class Task01  {
 		em.getTransaction().commit();
 		em.close();
 		//TODO under this line: create a second entity manager in categoryTest, use find method to find the category and assert its name.
+
+		em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Category foundCategory = em.find(Category.class, cat.getId());
+		em.getTransaction().commit();
+		em.close();
+
+		assertEquals(cat.getName(), foundCategory.getName());
+
 	}
 
 }
--- eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
+++ eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
@@ -21,9 +21,108 @@ import cz.fi.muni.pa165.entity.Product;
 @ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
 public class Task02 extends AbstractTestNGSpringContextTests {
 
+	private Category electro;
+	private Category kitchen;
+	private Product flashlight;
+	private Product kitchenRobot;
+	private Product plate;
+
+
 	@PersistenceUnit
 	private EntityManagerFactory emf;
 
+	@BeforeClass
+	private void beforeClass() {
+		electro = new Category();
+		electro.setName("Electro");
+		kitchen = new Category();
+		kitchen.setName("Kitchen");
+
+		flashlight = new Product();
+		flashlight.setName("Flashlight");
+		kitchenRobot = new Product();
+		kitchenRobot.setName("Kitchen robot");
+		plate = new Product();
+		plate.setName("Plate");
+
+		flashlight.addCategory(electro);
+		kitchenRobot.addCategory(electro);
+		kitchenRobot.addCategory(kitchen);
+		plate.addCategory(kitchen);
+
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(electro);
+		entityManager.persist(kitchen);
+		entityManager.persist(flashlight);
+		entityManager.persist(kitchenRobot);
+		entityManager.persist(plate);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Test
+	public void testCategoryElectro() {
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		Category foundCategory = entityManager.find(Category.class, electro.getId());
+
+		assertContainsProductWithName(foundCategory.getProducts(), flashlight.getName());
+		assertContainsProductWithName(foundCategory.getProducts(), kitchenRobot.getName());
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Test
+	public void testCategoryKitchen() {
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		Category foundCategory = entityManager.find(Category.class, kitchen.getId());
+
+		assertContainsProductWithName(foundCategory.getProducts(), plate.getName());
+		assertContainsProductWithName(foundCategory.getProducts(), kitchenRobot.getName());
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Test
+	public void testProductFlashlight() {
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		Product foundProduct = entityManager.find(Product.class, flashlight.getId());
+
+		assertContainsCategoryWithName(foundProduct.getCategories(), electro.getName());
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Test
+	public void testProductKitchenRobot() {
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		Product foundProduct = entityManager.find(Product.class, kitchenRobot.getId());
+
+		assertContainsCategoryWithName(foundProduct.getCategories(), kitchen.getName());
+		assertContainsCategoryWithName(foundProduct.getCategories(), electro.getName());
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Test
+	public void testProductPlate() {
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		Product foundProduct = entityManager.find(Product.class, plate.getId());
+
+		assertContainsCategoryWithName(foundProduct.getCategories(), kitchen.getName());
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
 	
 	private void assertContainsCategoryWithName(Set<Category> categories,
 			String expectedCategoryName) {
